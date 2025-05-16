005f1434feb78fa5ceeee41e76e6d0d73b9c94fd
mxTech1996 <168294592+mxTech1996@users.noreply.github.com>
2025-05-16 17:04:11 -0600

    Add src/data/index.js

---
+++ src/data/index.js
@@ -0,0 +1,42 @@
+export const pageName = 'AKWC';
+export const primaryColor = '#000000';
+export const backgroundColor = '#FFFFFF';
+export const makeClearBackground = (color) => {
+  return {
+    backgroundColor: color,
+  };
+};
+
+export const navData = [
+  {
+    href: '/#courses',
+    label: 'Our Course',
+  },
+  {
+    href: '/#our-services',
+    label: 'Our Services',
+  },
+  {
+    href: '/#references',
+    label: 'References',
+  },
+  {
+    href: '/#know-us',
+    label: 'Know Us',
+  },
+  {
+    href: '/more-information',
+    label: 'Contact Us',
+  },
+];
+
+export const footerData = [
+  {
+    href: '/pdf/AP.pdf',
+    label: 'Privacy',
+  },
+  {
+    href: '/pdf/TYC.pdf',
+    label: 'Terms',
+  },
+];
